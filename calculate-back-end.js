42160da5a15250100836be9a2abad968a97221cf
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2020-06-23 03:24:01 -0400

    all 16 passed

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -1,7 +1,25 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function calculate(string1, string2, operation) {
+  let num1 = parseFloat(string1);
+  let num2 = parseFloat(string2);
 
+  if (operation === "+" || operation === "plus" || operation === "added to") {
+      return num1 + num2;
+  } else if (operation === "-" || operation === "minus" || operation === "subtracted from") {
+      return num1 - num2;
+  } else if (operation === "x" || operation === "X" || operation === "times" || operation === "multiplied by") {
+      return num1 * num2;
+  } else if (operation === "/" || operation === "divided by") {
+      return num1 / num2;
+  } else if (operation === "%" || operation === "modulus" || operation === "mod") {
+      return num1 % num2;
+  } else {
+    return `Sorry, that's not a mathematical operation!`
+  }
+  
+}
 
  
 
